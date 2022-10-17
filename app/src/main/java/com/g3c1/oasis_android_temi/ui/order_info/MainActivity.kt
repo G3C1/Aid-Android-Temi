04436ac2d4ad896350f929cc68f6da74ea9239ae
khs3994 <z01039948003@gmail.com>
2022-10-17 22:19:03 +0900

    :sparkles: :: anotation 추가

--- app/src/main/java/com/g3c1/oasis_android_temi/ui/order_info/MainActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android_temi/ui/order_info/MainActivity.kt
@@ -3,7 +3,9 @@ package com.g3c1.oasis_android_temi.ui.order_info
 import com.g3c1.oasis_android_temi.R
 import com.g3c1.oasis_android_temi.base.BaseActivity
 import com.g3c1.oasis_android_temi.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun init() {
 
