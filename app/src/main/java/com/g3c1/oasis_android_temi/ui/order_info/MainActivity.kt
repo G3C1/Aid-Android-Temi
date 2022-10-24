f4c31d5dbcd7f6923e99c1eb94c357f49cb5613b
khs3994 <z01039948003@gmail.com>
2022-10-24 14:20:34 +0900

    ✨ :: 주문 목록 퍼블리싱

--- app/src/main/java/com/g3c1/oasis_android_temi/ui/order_info/MainActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android_temi/ui/order_info/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.g3c1.oasis_android_temi.ui.order_info
 
-import androidx.activity.viewModels
 import com.g3c1.oasis_android_temi.R
 import com.g3c1.oasis_android_temi.base.BaseActivity
 import com.g3c1.oasis_android_temi.databinding.ActivityMainBinding
-import com.g3c1.oasis_android_temi.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
-    private val vm by viewModels<MainViewModel>()
-
     override fun init() {
-         onClick()
-    }
-
-    private fun onClick() {
 
     }
 }
