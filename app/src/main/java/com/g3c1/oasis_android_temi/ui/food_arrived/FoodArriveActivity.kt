9c73254d107ecd62edc45519869f977e1629606a
khs3994 <z01039948003@gmail.com>
2022-10-26 23:25:41 +0900

    :sparkles: :: 도착 페이지 ui 구현

--- app/src/main/java/com/g3c1/oasis_android_temi/ui/food_arrived/FoodArriveActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android_temi/ui/food_arrived/FoodArriveActivity.kt
@@ -4,7 +4,7 @@ import com.g3c1.oasis_android_temi.R
 import com.g3c1.oasis_android_temi.base.BaseActivity
 import com.g3c1.oasis_android_temi.databinding.ActivityFoodBinding
 
-class FoodActivity : BaseActivity<ActivityFoodBinding>(R.layout.activity_food) {
+class FoodArriveActivity : BaseActivity<ActivityFoodBinding>(R.layout.activity_food) {
     override fun init() {
 
     }
