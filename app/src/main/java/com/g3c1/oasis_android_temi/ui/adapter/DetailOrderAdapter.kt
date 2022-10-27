cf1bfb34b3e3bdc3a2d098bc42cdda0210e85969
khs3994 <z01039948003@gmail.com>
2022-10-27 23:20:11 +0900

    :sparkles: :: 주문 목록 ui

---
+++ app/src/main/java/com/g3c1/oasis_android_temi/ui/adapter/DetailOrderAdapter.kt
@@ -0,0 +1,52 @@
+package com.g3c1.oasis_android_temi.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.g3c1.oasis_android_temi.databinding.OrderListItemItemBinding
+import com.g3c1.oasis_android_temi.ui.adapter.dto.FoodInfo
+
+class DetailOrderAdapter :
+    ListAdapter<FoodInfo, DetailOrderAdapter.DetailOrderViewHolder>(diffCallBack) {
+    class DetailOrderViewHolder(private val binding: OrderListItemItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: FoodInfo) {
+            binding.foodName.text = data.food
+            binding.foodCount.text = data.foodCnt.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailOrderViewHolder {
+        return DetailOrderViewHolder(
+            OrderListItemItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: DetailOrderViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffCallBack = object : DiffUtil.ItemCallback<FoodInfo>() {
+            override fun areItemsTheSame(
+                oldItem: FoodInfo,
+                newItem: FoodInfo
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: FoodInfo,
+                newItem: FoodInfo
+            ): Boolean {
+                return oldItem.food == newItem.food && oldItem.foodCnt == newItem.foodCnt
+            }
+        }
+    }
+}
--- app/src/main/java/com/g3c1/oasis_android_temi/ui/order_info/MainActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android_temi/ui/order_info/MainActivity.kt
@@ -1,13 +1,56 @@
 package com.g3c1.oasis_android_temi.ui.order_info
 
+import androidx.recyclerview.widget.GridLayoutManager
 import com.g3c1.oasis_android_temi.R
 import com.g3c1.oasis_android_temi.base.BaseActivity
 import com.g3c1.oasis_android_temi.databinding.ActivityMainBinding
+import com.g3c1.oasis_android_temi.ui.util.ItemDecorator
+import com.g3c1.oasis_android_temi.ui.adapter.OrderAdapter
+import com.g3c1.oasis_android_temi.ui.adapter.dto.FoodInfo
+import com.g3c1.oasis_android_temi.ui.adapter.dto.OrderInfo
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun init() {
 
+    private val orderAdapter = OrderAdapter()
+    private val orderList = mutableListOf(
+        OrderInfo(
+            mutableListOf(
+                FoodInfo("후라이드 치킨", 2),
+                FoodInfo("후라이드 치킨", 2),
+                FoodInfo("후라이드 치킨", 2)
+            ), 1
+        ),
+        OrderInfo(
+            mutableListOf(
+                FoodInfo("후라이드 치킨", 4),
+                FoodInfo("후라이드 치킨", 2),
+                FoodInfo("후라이드 치킨", 2)
+            ), 2
+        ),
+        OrderInfo(mutableListOf(FoodInfo("후라이드 치킨", 7)), 3),
+        OrderInfo(mutableListOf(FoodInfo("후라이드 치킨", 2)), 4),
+        OrderInfo(mutableListOf(FoodInfo("후라이드 치킨", 3)), 5),
+        OrderInfo(mutableListOf(FoodInfo("후라이드 치킨", 1)), 6),
+    )
+
+    override fun init() {
+        settingRecycler()
+    }
+
+    fun settingRecycler() {
+        with(binding.orderRecycler) {
+            layoutManager = GridLayoutManager(
+                context,
+                2,
+                GridLayoutManager.VERTICAL,
+                false
+            )
+            addItemDecoration(ItemDecorator(90, "VERTICAL"))
+            setHasFixedSize(true)
+            binding.orderRecycler.adapter = orderAdapter
+            orderAdapter.submitList(orderList)
+        }
     }
 }
