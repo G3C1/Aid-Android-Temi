bda7922d3e3333493072ed105deb16824cbcad30
khs3994 <z01039948003@gmail.com>
2022-12-05 23:29:34 +0900

    :sparkles: :: 스플래쉬 화면 퍼블리싱

---
+++ app/src/main/java/com/g3c1/oasis_android_temi/presentation/ui/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.g3c1.oasis_android_temi.presentation.ui.splash
+
+import com.g3c1.oasis_android_temi.R
+import com.g3c1.oasis_android_temi.databinding.ActivitySplashBinding
+import com.g3c1.oasis_android_temi.presentation.base.BaseActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun init() {
+
+    }
+}
