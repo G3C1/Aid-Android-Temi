fec57066feb05ea735219b27a853ca1343e3c011
khs3994 <z01039948003@gmail.com>
2022-12-06 10:00:40 +0900

    :sparkles: :: QR이 스캔 되어있다면 Main으로 안되어 있다면 QR스캔페이지로 가는 로직 추가

--- app/src/main/java/com/g3c1/oasis_android_temi/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/g3c1/oasis_android_temi/presentation/ui/splash/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.g3c1.oasis_android_temi.presentation.ui.splash
 
+import android.content.Intent
 import com.g3c1.oasis_android_temi.R
 import com.g3c1.oasis_android_temi.databinding.ActivitySplashBinding
+import com.g3c1.oasis_android_temi.di.TemiApplication
 import com.g3c1.oasis_android_temi.presentation.base.BaseActivity
+import com.g3c1.oasis_android_temi.presentation.ui.orderinfo.MainActivity
+import com.g3c1.oasis_android_temi.presentation.ui.qrscan.QrScanActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun init() {
-
+        CoroutineScope(Dispatchers.Main).launch {
+            if (TemiApplication.getInstance()
+                    .getSearialNumberManager().searialNumber.first() == 0
+            ) {
+                startActivity(Intent(this@SplashActivity, QrScanActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        }
     }
 }
